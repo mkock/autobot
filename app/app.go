b170db1fc07720e98d9ac253c3667b59ad52a7b6
Martin Kock <mk@omnicar.dk>
2018-10-30 22:26:08 +0100

    Refactor commands using go-flags

---
+++ app/app.go
@@ -0,0 +1,208 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"time"
+
+	"github.com/OmniCar/autobot/config"
+	"github.com/OmniCar/autobot/dataprovider"
+	"github.com/OmniCar/autobot/dmr"
+	"github.com/OmniCar/autobot/vehicle"
+	"github.com/OmniCar/autobot/vehiclestore"
+	"github.com/jessevdk/go-flags"
+)
+
+var (
+	parser     *flags.Parser              // Initialised in init.
+	globalOpts *Options                   // Initialised in init.
+	conf       config.Config              // Initialised in bootstrap.
+	store      *vehiclestore.VehicleStore // Initialised in bootstrap.
+)
+
+// init (called automatically) sets up the CLI parser.
+func init() {
+	// Setup commands.
+	var (
+		syncCmd   SyncCommand
+		clearCmd  ClearCommand
+		statusCmd StatusCommand
+		lookupCmd LookupCommand
+	)
+	globalOpts = &Options{}
+	parser = flags.NewParser(globalOpts, flags.Default)
+	parser.CommandHandler = bootstrap // Loads conf and connects to the vehicle store before executing commands.
+	parser.AddCommand("sync", "synchronise", "synchronise the vehicle store with an external data source", &syncCmd)
+	parser.AddCommand("clear", "clear", "clears the vehicle store of all vehicles", &clearCmd)
+	parser.AddCommand("status", "status", "displays a short status of the vehicle store", &statusCmd)
+	parser.AddCommand("lookup", "vehicle lookup", "performs a vehicle lookup, by VIN or registration number", &lookupCmd)
+}
+
+func monitorRuntime() {
+	log.Println("Number of CPUs:", runtime.NumCPU())
+	m := &runtime.MemStats{}
+	for {
+		r := runtime.NumGoroutine()
+		log.Println("Number of goroutines", r)
+		runtime.ReadMemStats(m)
+		log.Println("Allocated memory:", m.Alloc)
+		time.Sleep(10 * time.Second)
+	}
+}
+
+// Options contains command-line arguments parsed upon application initialisation.
+type Options struct {
+	ConfigFile string `short:"c" long:"config-file" required:"yes" default:"config.toml" description:"Application configuration file in TOML format"`
+}
+
+// SyncCommand contains options for synchronising the vehicle store with an external source.
+type SyncCommand struct {
+	SourceFile string `short:"f" long:"source-file" description:"DMR XML file in UTF-8 format"`
+	Debug      bool   `short:"d" long:"debug" description:"Debug: print CPU count, goroutine count and memory usage every 10 seconds"`
+}
+
+// Execute runs the command.
+func (cmd *SyncCommand) Execute(opts []string) error {
+	fmt.Println("Executing SyncCommand!")
+	if cmd.Debug {
+		go monitorRuntime()
+	}
+	var ptype int
+	if cmd.SourceFile == "" {
+		fmt.Printf("Using FTP data file at %q\n", conf.Ftp.Host)
+		ptype = dataprovider.FtpProv
+	} else {
+		fmt.Printf("Using local data file: %s\n", cmd.SourceFile)
+		ptype = dataprovider.FsProv
+	}
+
+	prov := dataprovider.NewProvider(ptype, conf)
+	src, err := prov.Provide(cmd.SourceFile)
+	if err != nil {
+		return err
+	}
+	if src == nil {
+		log.Print("No stat file detected. Aborting.")
+		return nil
+	}
+
+	id := store.NewSyncOp(dataprovider.ProvTypeString(ptype))
+
+	dmrService := dmr.NewService()
+	vehicles, done := dmrService.LoadNew(src)
+	if err := store.Sync(id, vehicles, done); err != nil {
+		return err
+	}
+	fmt.Println(store.Status(id))
+
+	return nil
+}
+
+// ClearCommand contains options for clearing the vehicle store.
+type ClearCommand struct {
+	Clear bool `short:"l" long:"clear" description:"Clears the entire vehicle store"`
+}
+
+// Execute runs the command.
+func (cmd *ClearCommand) Execute(opts []string) error {
+	fmt.Println("Executing ClearCommand!")
+	if err := store.Clear(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// StatusCommand contains options for displaying the status of the vehicle store.
+type StatusCommand struct {
+	Status bool `short:"s" long:"status" description:"Displays the last synchronisation log"`
+}
+
+// Execute runs the command.
+func (cmd *StatusCommand) Execute(opts []string) error {
+	fmt.Println("Executing StatusCommand!")
+	entries, err := store.CountLog()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Status: %d log entries in total. Last entry:\n", entries)
+	entry, err := store.LastLog()
+	if err != nil {
+		return err
+	}
+	fmt.Println(entry.String())
+	return nil
+}
+
+// LookupCommand contains options for vehicle lookups using reg.nr. or VIN.
+type LookupCommand struct {
+	VIN   string `short:"v" long:"vin" description:"VIN number to lookup, if any (will not synchronize data)"`
+	RegNr string `short:"r" long:"regnr" description:"Registration number to lookup, if any (will not synchronize data)"`
+}
+
+// Execute is called by go-flags and thus bootstraps the LookupCommand.
+func (cmd *LookupCommand) Execute(opts []string) error {
+	fmt.Println("Executing LookupCommand!")
+	var (
+		nr     string
+		desc   string
+		lookup func(string) (vehicle.Vehicle, error)
+	)
+	if cmd.RegNr != "" {
+		nr = cmd.RegNr
+		desc = "registration number"
+		lookup = store.LookupByRegNr
+	} else if cmd.VIN != "" {
+		lookup = store.LookupByVIN
+		desc = "VIN"
+		nr = cmd.VIN
+	} else {
+		fmt.Println("Lookup: need VIN or registration number")
+		return nil
+	}
+	veh, err := lookup(nr)
+	if err != nil {
+		return err
+	}
+	if veh == (vehicle.Vehicle{}) {
+		fmt.Printf("No vehicle found with %s %s", desc, nr)
+	}
+	fmt.Println("Found!")
+	fmt.Println(veh.FlexString("\n", "  "))
+	return nil
+}
+
+// loadConfig loads the TOML configuration file with the specified name.
+func loadConfig(fname string) (config.Config, error) {
+	return config.NewConfig(fname)
+}
+
+// bootstrap loads the app configuration and connects to the vehicle store.
+func bootstrap(cmd flags.Commander, args []string) error {
+	// Load the configuration file passed via the CLI.
+	var err error
+	conf, err = loadConfig(globalOpts.ConfigFile)
+	if err != nil {
+		return err
+	}
+	// Connect to the vehicle store.
+	store = vehiclestore.NewVehicleStore(conf.MemStore, conf.Sync)
+	if err := store.Open(); err != nil {
+		return err
+	}
+	defer func() {
+		store.Close()
+	}()
+
+	// Carry on with command execution.
+	return cmd.Execute(args)
+}
+
+// Start runs a command.
+func Start() error {
+	// This will run the command that matches the command-line options.
+	if _, err := parser.Parse(); err != nil {
+		return err
+	}
+	return nil
+}
--- cmd/autobot/main.go
+++ cmd/autobot/main.go
@@ -5,127 +5,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"runtime"
-	"time"
 
-	"github.com/OmniCar/autobot/args"
-	"github.com/OmniCar/autobot/config"
-	"github.com/OmniCar/autobot/dataprovider"
-	"github.com/OmniCar/autobot/dmr"
-	"github.com/OmniCar/autobot/vehicle"
-	"github.com/OmniCar/autobot/vehiclestore"
+	"github.com/OmniCar/autobot/app"
 )
 
-func monitorRuntime() {
-	log.Println("Number of CPUs:", runtime.NumCPU())
-	m := &runtime.MemStats{}
-	for {
-		r := runtime.NumGoroutine()
-		log.Println("Number of goroutines", r)
-		runtime.ReadMemStats(m)
-		log.Println("Allocated memory:", m.Alloc)
-		time.Sleep(10 * time.Second)
-	}
-}
-
 func main() {
-	// Parse CLI arguments.
-	args, err := args.ParseCLI()
-	if err != nil {
-		log.Fatalf("Failed to parse CLI arguments: %s", err)
+	if err := app.Start(); err != nil {
+		log.Fatalf("Error: %s", err)
 	}
-	if args.Debug {
-		go monitorRuntime()
-	}
-
-	// Load and parse configuration file.
-	if args.ConfigFile == "" {
-		log.Fatalf("Need a configuration file")
-	}
-	cnf, err := config.NewConfig(args.ConfigFile)
-	if err != nil {
-		log.Fatalf("Unable to load configuration file %s", args.ConfigFile)
-	}
-
-	store := vehiclestore.NewVehicleStore(cnf.MemStore, cnf.Sync)
-	if err := store.Open(); err != nil {
-		log.Fatalf("Unable to connect to memory store, check your configuration file")
-	}
-	defer func() {
-		store.Close()
-	}()
-	if args.VIN != "" {
-		veh, err := store.LookupByVIN(args.VIN)
-		if err != nil {
-			fmt.Printf("Unable to lookup VIN %s: %s\n", args.VIN, err)
-			os.Exit(0)
-		}
-		if veh == (vehicle.Vehicle{}) {
-			fmt.Printf("No vehicle found with VIN %s", args.VIN)
-			os.Exit(0)
-		}
-		fmt.Println("Found!")
-		fmt.Println(veh.FlexString("\n", "  "))
-		os.Exit(0)
-	} else if args.RegNr != "" {
-		veh, err := store.LookupByRegNr(args.RegNr)
-		if err != nil {
-			fmt.Printf("Unable to lookup reg. nr. %s: %s\n", args.RegNr, err)
-			os.Exit(0)
-		}
-		if veh == (vehicle.Vehicle{}) {
-			fmt.Printf("No vehicle found with registration number %s", args.RegNr)
-			os.Exit(0)
-		}
-		fmt.Println("Found!")
-		fmt.Println(veh.FlexString("\n", "  "))
-		os.Exit(0)
-	} else if args.Clear {
-		if err := store.Clear(); err != nil {
-			log.Fatal("Unable clear store, manual cleanup required\n")
-		}
-		fmt.Println("Store cleared")
-		os.Exit(0)
-	} else if args.Status {
-		entries, err := store.CountLog()
-		if err != nil {
-			log.Fatalf("Error while fetching status: %s", err)
-		}
-		fmt.Printf("Status: %d log entries in total. Last entry:\n", entries)
-		entry, err := store.LastLog()
-		if err != nil {
-			log.Fatalf("Error while fetching status: %s", err)
-		}
-		fmt.Println(entry.String())
-		os.Exit(0)
-	}
-
-	var ptype int
-	if args.SourceFile == "" {
-		fmt.Printf("Using FTP data file at %q\n", cnf.Ftp.Host)
-		ptype = dataprovider.FtpProv
-	} else {
-		fmt.Printf("Using local data file: %s\n", args.SourceFile)
-		ptype = dataprovider.FsProv
-	}
-
-	prov := dataprovider.NewProvider(ptype, cnf)
-	src, err := prov.Provide(args.SourceFile)
-	if err != nil {
-		log.Fatalf("error during file retrieval: %s", err)
-	}
-	if src == nil {
-		log.Print("No stat file detected. Quitting.")
-		os.Exit(0)
-	}
-
-	id := store.NewSyncOp(dataprovider.ProvTypeString(ptype))
-
-	dmrService := dmr.NewService()
-	vehicles, done := dmrService.LoadNew(src)
-	if err := store.Sync(id, vehicles, done); err != nil {
-		log.Fatalf("error during sync: %s", err)
-	}
-	fmt.Println(store.Status(id))
+	fmt.Println("Done.")
 }
